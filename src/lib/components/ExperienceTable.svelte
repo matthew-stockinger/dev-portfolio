fec854d44cf7f257c6bcb6228607f663e2fdfd71
Matthew Stockinger <matt@matthewstockinger.com>
2026-01-02 08:45:50 -0600

    complete 51

--- src/lib/components/ExperienceTable.svelte
+++ src/lib/components/ExperienceTable.svelte
@@ -54,5 +54,39 @@
 			</li>
 		{/each}
 	</ul>
-	<h2 class="healine">Past Work <br /> Experience</h2>
+	<h2 class="headline">Past Work <br /> Experience</h2>
 </section>
+
+<style>
+	.work-experience {
+		display: flex;
+		justify-content: space-between;
+		width: 100%;
+	}
+
+	.work-experience-list {
+		width: 50%;
+	}
+
+	.headline {
+		text-align: right;
+	}
+
+	.work-item {
+		border-bottom: 1px solid #f0eded;
+		padding-bottom: 12px;
+	}
+
+	.work-item:not(:first-of-type) {
+		padding-top: 16px;
+	}
+
+	.work-item p {
+		margin-bottom: 0;
+	}
+
+	.company-and-date {
+		display: flex;
+		justify-content: space-between;
+	}
+</style>
