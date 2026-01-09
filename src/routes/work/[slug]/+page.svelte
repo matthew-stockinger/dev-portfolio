f5383ffabc9e8d41b7f3c1c576a1f2b3cc4f5626
Matthew Stockinger <matt@matthewstockinger.com>
2026-01-08 18:50:58 -0600

    complete 62

--- src/routes/work/[slug]/+page.svelte
+++ src/routes/work/[slug]/+page.svelte
@@ -40,3 +40,28 @@
 		</div>
 	</div>
 </main>
+
+<style>
+	.work-page {
+		padding-top: 80px;
+		padding-bottom: 140px;
+	}
+
+	.project-image {
+		width: 100%;
+		max-height: 450px;
+		object-fit: cover;
+	}
+
+	.content-image {
+		width: 100%;
+	}
+
+	.project-container {
+		display: flex;
+	}
+
+	.meta-data {
+		min-width: 200px;
+	}
+</style>
