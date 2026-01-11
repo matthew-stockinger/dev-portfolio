6ae02d41d33b8c90d1c658cb7165478bbc9d98f1
Matthew Stockinger <matt@matthewstockinger.com>
2026-01-11 09:54:02 -0600

    complete 65

--- src/lib/components/Sections/SkillsSection.svelte
+++ src/lib/components/Sections/SkillsSection.svelte
@@ -10,11 +10,33 @@
 
 <section class="mt-l">
 	<SectionHeadline sectionName="skills">Skills</SectionHeadline>
-    <div class="wrapper default-margin">
-        <div class="skills-container mt-m">
-            {#each skills as skill}
-                <i class={skill.iconClass}></i>
-            {/each}
-        </div>
-    </div>
+	<div class="wrapper default-margin">
+		<div class="skills-container mt-m">
+			{#each skills as skill}
+				<i class={skill.iconClass}></i>
+			{/each}
+		</div>
+	</div>
 </section>
+
+<style>
+	.wrapper {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.skills-container {
+		width: 40%;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		row-gap: 16px;
+		column-gap: 10px;
+	}
+
+	i {
+		font-size: 80px;
+		color: black;
+	}
+</style>
