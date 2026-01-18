b4643132204c2a5ff20518efcaef716b52fe2a97
Matthew Stockinger <matt@matthewstockinger.com>
2026-01-17 21:15:53 -0600

    complete 71

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -14,3 +14,28 @@
         <Button className="nav-bar" onclick={goToContactForm}>Contact</Button>
     </div>
 </nav>
+
+<style>
+    .navbar {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 20px;
+    }
+
+    .logo {
+        font-size: 35px;
+        font-weight: bold;
+    }
+
+    .navbar-links {
+        display: flex;
+        align-items: center;
+        gap: 60px;
+    }
+
+    .nav-link:hover {
+        text-decoration: underline;
+    }
+
+</style>
